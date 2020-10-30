0689c7b1500ab2117f10d0b230b1204fca886b30
take <takehiro.arai.jg@gmail.com>
2020-10-30 16:39:33 +0900

    Add login action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:rassword])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -12,6 +15,5 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
   end
 end
