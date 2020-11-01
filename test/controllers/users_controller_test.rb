fb3f12c52c3b3decbe3b634510892823af9bfa7a
take <takehiro.arai.jg@gmail.com>
2020-11-01 14:14:16 +0900

    10.3

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
 
   test "should get new" do
     get signup_path
@@ -40,5 +45,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+
   test "layout links" do
     get root_path
     assert_template "static_pages/home"
@@ -13,5 +19,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links when logged in user" do
+    log_in(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path, count: 1
+    assert_select "a[href=?]", about_path, count: 1
+    assert_select "a[href=?]", contact_path, count: 1
+    assert_select "a[href=?]", signup_path, count: 1
+    assert_select "a[href=?]", users_path, count: 1
+    assert_select "a[href=?]", user_path(@user), count: 1
+    assert_select "a[href=?]", edit_user_path(@user), count: 1
+    assert_select "a[href=?]", logout_path, count: 1
   end
 end
