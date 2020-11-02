9766890d4ac9759277001c16de201fa8ed96108d
take <takehiro.arai.jg@gmail.com>
2020-11-03 08:49:04 +0900

    recreate app on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://afternoon-hollows-73404.herokuapp.com'
+  host = 'https://blooming-savannah-15408.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
